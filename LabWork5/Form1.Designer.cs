d95037c127832f97d521ed82cd268b2b3e88da1c
waksimusss <nimaksimov@edu.hse.ru>
2022-11-29 20:17:06 +0500

    Добавьте файлы проекта.

---
+++ LabWork5/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace LabWork5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(852, 524);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label2.Location = new System.Drawing.Point(635, 487);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 22);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Кол-во строк";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label1.Location = new System.Drawing.Point(635, 450);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 22);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Кол-во столбцов";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.textBox3.Location = new System.Drawing.Point(529, 474);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 38);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.textBox2.Location = new System.Drawing.Point(529, 437);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 38);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(722, 191);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 58);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Добавить строку";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(299, 437);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(224, 75);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Сгенерировать массив из случайных чисел";
+            this.button4.UseVisualStyleBackColor = true;
+            //this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(24, 437);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(224, 75);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Работа с рваным массивом";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 75);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Работа с двумерным массивом";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 75);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Работа с динамическим массивом";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(852, 185);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 524);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабараторная работа 5";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ LabWork5/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+
+namespace LabWork5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void isCorrect()
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "Привет! Давайте уже начнем.";
+        }
+
+        private string[][] ParseText(ref string text)
+        {
+            string[] strings = text.Split('\n');
+            string[][] lastStr = new string[strings.Length][];
+            for (int i = 0; i < strings.Length; i++)
+            {
+                string[] stroka = strings[i].Split(' ');
+                lastStr[i] = stroka;
+            }
+            return lastStr;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            delSrarif(ref str);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 44 && e.KeyChar != 8 && e.KeyChar != 32 && e.KeyChar != 13)
+            {
+                e.Handled = true;
+            }   
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8 && e.KeyChar != 44 && e.KeyChar != 45)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string newLine = Environment.NewLine;
+            textBox1.Text += newLine;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /* private void button4_Click(object sender, EventArgs e)
+         {
+            int masString, masColumn;
+
+             Random rnd = new Random();
+             if ((textBox2.Text == "") || (textBox3.Text == ""))
+                 textBox1.Text = "Введите целое число для создания массива из случайных чисел";
+             else
+             {
+                 if (textBox3.Text == "0")
+                 {
+                     int[] dinMas = new int[masColumn];
+                     for (int i = 0; i < masColumn; i++)
+                     {
+
+                         dinMas[i] = rnd.Next(-100,100);
+                     }
+                     textBox1.Text = string.Join(" ", dinMas);
+                 }
+
+             }
+         }*/
+        private void checkEmpty(ref string[][] text)
+        {
+            if (text.Length == 0)
+            {
+                MessageBox.Show("Пустой массив []");
+            }
+        }
+        private void delSrarif(ref string Solve)
+        {
+            
+            string messageText = "";
+            
+            
+            string text = textBox1.Text;
+            string[][] array = ParseText(ref text);
+            checkEmpty(ref array);
+            int[] ints = new int[array[0].Length];
+            for (int i = 0; i < ints.Length; i++)
+            {
+                ints[i] = Convert.ToInt32(array[0][i]);
+            }
+           
+            double srArif = 0d;
+            int indexElem = -100;
+            for (int i = 0; i < ints.Length - 1; i++)
+                srArif += ints[i];
+            srArif /= ints.Length;
+            
+            for (int i = 0; i < ints.Length; i++)
+            {
+
+                if (ints[i] == Math.Round(srArif))
+                {
+                    indexElem = i;
+                    break;
+                }
+            }
+            if (indexElem == -100)
+            {
+                messageText += $"В массиве нет элемента, равного среднему арфметическому всех чисел в массиве.";
+                MessageBox.Show(messageText);
+            }
+            else
+            {
+                int[] newArray = new int[ints.Length - 1];
+                for (int i = 0; i < indexElem; i++)
+                    newArray[i] = ints[i];
+                for (int i = indexElem + 1; i < ints.Length; i++)
+                    newArray[i] = ints[i];
+                messageText += $"{ints} Был удален элемент {ints[indexElem]}";
+                MessageBox.Show(messageText);
+                for (int i = 0; i<newArray.Length;i++)
+                {
+                    messageText += Convert.ToString(newArray[i]);
+                }
+                textBox1.Text = messageText;
+            }
+            
+        }
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textBox1.Text == "Привет! Давайте уже начнем.")
+            {
+                textBox1.Text = "";
+
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
